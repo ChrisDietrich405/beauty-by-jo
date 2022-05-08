5b3f0901e4e1526d42dcdb106a79d9584ff6f7c5
Chris <chrisdietrich3666@gmail.com>
2022-05-07 19:28:51 -0500

    added toastify for alerts

--- src/components/CreateAccount.js
+++ src/components/CreateAccount.js
@@ -1,139 +1,143 @@
 import { bindActionCreators } from "redux";
-import { connect } from "react-redux";
+import { connect, useSelector } from "react-redux";
 import { Formik, ErrorMessage } from "formik";
+import { ToastContainer, toast } from "react-toastify";
+import { useHistory } from "react-router-dom";
+
+import "react-toastify/dist/ReactToastify.css";
 
 import { save } from "../store/actions/user";
 
 import "../styles/components/sign-in-create-account.scss";
+import { useEffect } from "react";
 
 function CreateAccount({ user, save }) {
-  // const [firstName, setFirstName] = useState("");
-  // const [lastName, setLastName] = useState("");
-  // const [email, setEmail] = useState("");
-  // const [password, setPassword] = useState("");
+  const notify = () => toast.success("Your account has been created");
+  const { user: currentUser } = useSelector((state) => state.user);
+  const history = useHistory();
 
-  // const submit = (e) => {
-  //   e.preventDefault();
-  // };
+  useEffect(() => {
+    if (currentUser.firstName !== null) {
+      notify();
+      setTimeout(() => {
+        history.push("/signin");
+      }, 6000);
+    }
+  }, [currentUser]);
 
   return (
-    <Formik
-      initialValues={{
-        firstName: "",
-        lastName: "",
-        email: "",
-        password: "",
-      }}
-      validate={(values) => {
-        console.log("Validate", values);
-        const errors = {};
-        if (!values.firstName.trim()) {
-          errors.firstName = "First name is required";
-        }
-        if (!values.lastName.trim()) {
-          errors.lastName = "Last name is required";
-        }
-        if (!values.email.trim()) {
-          errors.email = "Email required";
-        } else if (
-          !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-        ) {
-          errors.email = "Invalid email address";
-        }
-        if (!values.password.trim()) {
-          errors.password = "Password is required";
-        }
-        console.log(errors);
-        return errors;
-      }}
-      onSubmit={(values, { setSubmitting }) => {
-        save(values);
-        //console.log(values);
-        //   setTimeout(() => {
-        //     alert(JSON.stringify(values, null, 2));
-        //     setSubmitting(false);
-        //   }, 400);
-      }}
-    >
-      {({
-        values,
-        errors,
-        touched,
-        handleChange,
-        handleBlur,
-        handleSubmit,
-        isSubmitting,
-        /* and other goodies */
-      }) => (
-        <div className="form-container">
-          {JSON.stringify({
-            values,
-            errors,
-            touched,
-            handleChange,
-            handleBlur,
-            handleSubmit,
-            isSubmitting,
-            /* and other goodies */
-          })}
-          <form onSubmit={handleSubmit}>
-            <h2>Create account</h2>
-            <label htmlFor="firstName">
-              First Name
-              <input
-                type="text"
-                name="firstName"
-                id="firstName"
-                value={values.firstName}
-                onChange={handleChange}
-                onBlur={handleBlur}
-              />
-              <ErrorMessage name="firstName" />
-            </label>
-            <label htmlFor="lastName">
-              Last Name
-              <input
-                type="text"
-                name="lastName"
-                id="lastName"
-                value={values.lastName}
-                onChange={handleChange}
-                onBlur={handleBlur}
-              />
-              <ErrorMessage name="lastName" />
-            </label>
-            <label htmlFor="email">
-              Email
-              <input
-                type="text"
-                name="email"
-                id="email"
-                value={values.email}
-                onChange={handleChange}
-                onBlur={handleBlur}
-              />
-              <ErrorMessage name="email" />
-            </label>
-            <label htmlFor="password">
-              Password
-              <input
-                type="password"
-                name="password"
-                id="password"
-                value={values.password}
-                onChange={handleChange}
-                onBlur={handleBlur}
-              />
-              <ErrorMessage name="password" />
-            </label>
+    <>
+      <ToastContainer />
+      <Formik
+        initialValues={{
+          firstName: "",
+          lastName: "",
+          email: "",
+          password: "",
+        }}
+        validate={(values) => {
+          const errors = {};
+          if (!values.firstName.trim()) {
+            errors.firstName = "First name is required";
+          }
+          if (!values.lastName.trim()) {
+            errors.lastName = "Last name is required";
+          }
+          if (!values.email.trim()) {
+            errors.email = "Email required";
+          } else if (
+            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+          ) {
+            errors.email = "Invalid email address";
+          }
+          if (!values.password.trim()) {
+            errors.password = "Password is required";
+          }
 
-            <button type="submit" className="sign-in-button">
-              Submit
-            </button>
-          </form>
-        </div>
-      )}
-    </Formik>
+          return errors;
+        }}
+        onSubmit={(values, { setSubmitting }) => {
+          save(values);
+        }}
+      >
+        {({
+          values,
+          errors,
+          touched,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          isSubmitting,
+        }) => (
+          <div className="form-container">
+            {JSON.stringify({
+              values,
+              errors,
+              touched,
+              handleChange,
+              handleBlur,
+              handleSubmit,
+              isSubmitting,
+            })}
+            <form onSubmit={handleSubmit}>
+              <h2>Create account</h2>
+              <label htmlFor="firstName">
+                First Name
+                <input
+                  type="text"
+                  name="firstName"
+                  id="firstName"
+                  value={values.firstName}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+                <ErrorMessage name="firstName" />
+              </label>
+              <label htmlFor="lastName">
+                Last Name
+                <input
+                  type="text"
+                  name="lastName"
+                  id="lastName"
+                  value={values.lastName}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+                <ErrorMessage name="lastName" />
+              </label>
+              <label htmlFor="email">
+                Email
+                <input
+                  type="text"
+                  name="email"
+                  id="email"
+                  value={values.email}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+                <ErrorMessage name="email" />
+              </label>
+              <label htmlFor="password">
+                Password
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  value={values.password}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                />
+                <ErrorMessage name="password" />
+              </label>
+
+              <button type="submit" className="sign-in-button">
+                Submit
+              </button>
+            </form>
+          </div>
+        )}
+      </Formik>
+    </>
   );
 }
 
@@ -145,28 +149,3 @@ const mapStateToProps = (state) => ({
 const mapDispatchToProps = (dispatch) => bindActionCreators({ save }, dispatch);
 
 export default connect(mapStateToProps, mapDispatchToProps)(CreateAccount);
-
-// <div className="form-container">
-//         <form onSubmit={submit}>
-//             <h4>Create account</h4>
-//             <label htmlFor="name">
-//                 First Name
-//                 <input type="text" id="name" value={firstName} onChange={(e) => setFirstName(e.target.value)}  />
-//             </label>
-//             <label htmlFor="name">
-//                 Last Name
-//                 <input type="text" id="name" value={lastName} onChange={(e) => setLastName(e.target.value)}  />
-//             </label>
-//             <label htmlFor="email">
-//                 Email
-//                 <input type="text" id="email" value={email} onChange={(e) => setEmail(e.target.value)}  />
-//             </label>
-//             <label htmlFor="password">
-//                 Password
-//                 <input type="text" placeholder="" id="password" value={password} onChange={(e) => setPassword(e.target.value)}  />
-//                 <p className="password-minimum">(At least 6 characters)</p>
-//             </label>
-//             <button type="submit" className="sign-in-button">Submit</button>
-//         </form>
-// </div>
-//)
