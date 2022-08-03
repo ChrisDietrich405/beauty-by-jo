ab4043db5da8ca186a900d8db13e9d90c1b0e110
Julio <julioccpv@gmail.com>
2022-08-03 15:05:03 -0300

    ForgotPassword removing unused vars and fixing dependencies

--- src/pages/ForgotPassword.js
+++ src/pages/ForgotPassword.js
@@ -1,7 +1,7 @@
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { Link, useHistory } from "react-router-dom";
 import { bindActionCreators } from "redux";
-import { connect, useSelector } from "react-redux";
+import { connect } from "react-redux";
 import { Formik, ErrorMessage } from "formik";
 
 import {
@@ -15,10 +15,7 @@ import "../styles/components/sign-in-create-account.scss";
 import { subscribe } from "../store";
 import { errorToast, successToast } from "../store/actions/toast";
 
-// import AppointmentModal from "./AppointmentModal";
-
 function ForgotPassword({ forgotPassword, successToast, errorToast }) {
-  const history = useHistory();
 
   useEffect(() => {
     const unsubscribeAuthSuccess = subscribe.on(FORGOT_PASSWORD_SUCCESS, () => {
@@ -37,7 +34,7 @@ function ForgotPassword({ forgotPassword, successToast, errorToast }) {
       unsubscribeAuthSuccess();
       unsubscribeAuthError();
     };
-  }, []);
+  }, [errorToast, successToast]);
 
   return (
     <>
