51dfd4846a18b8180f43f6677a84e9798ee7ac9c
Julio <julioccpv@gmail.com>
2022-08-04 14:06:46 -0300

    ForgotPassword unused vars

--- src/pages/ForgotPassword.js
+++ src/pages/ForgotPassword.js
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { Link, useHistory } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
 import { Formik, ErrorMessage } from "formik";
